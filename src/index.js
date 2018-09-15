6257e6e14edfbf664e64eb74115848e17d76148a
veterkam <veterkam@gmail.com>
2018-09-15 16:13:41 +0300

    Money-Exchange/feature: implement function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    if(currency <= 0) {
+        return {};
+    }
+    if(currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+
+    let coins = new Map ([
+        ['H', 50],
+        ['Q', 25],
+        ['D', 10],
+        ['N', 5],
+        ['P', 1]
+    ]);
+
+    let result = {};
+    for (let coin of coins.keys()) {
+        let nominal = coins.get(coin);
+        let count = Math.floor(currency / nominal);        
+        
+        if(count > 0) {
+            result[coin] = count;
+            currency -= nominal * count;
+        }        
+    }
+
+    return result;
 }
